52134f4197069b0cf620939562c331d2c228b592
Oleksandr Bondarchuk <56359969+bondar4uk@users.noreply.github.com>
2023-06-01 13:56:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 let gl;
 let surface;
+let surface2;
 let shProgram;
 let spaceball;
 let canvas;
@@ -14,9 +15,9 @@ let video_src = "texture/texture_vid.mp4";
 let copyVideo = false;
 let texture_type = "image";
 
-let World_X = -1;
+let World_X = 0;
 let World_Y = 0;
-let World_Z = -40;
+let World_Z = 0;
 
 let CanvasWidth;
 let CanvasHeight;
@@ -24,11 +25,50 @@ let video;
 let video_cam;
 let TextureWebCam;
 let SurfaceTexture;
+let SphereTexture;
 let BackgroundVideoModel;
+let Sphere;
 
 let image;
 
-let stereoCamera = new StereoCamera(5, 0.4, 1, 1, 4, 100);
+let stereoCamera = new StereoCamera(5, 0, 1, 1, 4, 100);
+
+let audio = null;
+let audioContext;
+let audioSource;
+let audioPanner;
+let audioFilter;
+let reverbNode;
+let centerFrequencyInput = 1000;
+let Q_value = 5;
+let Volume = 1;
+let PlaybackRate = 1;
+
+let timeStamp;
+let x,
+    y,
+    z;
+
+function readGyroscope() {
+    var gyroPresent = navigator.userAgentData.mobile;
+
+    if (gyroPresent) {
+        timeStamp = Date.now();
+        let sensor = new Gyroscope({
+            frequency: 60
+        });
+        sensor.addEventListener('reading', e => {
+            x = sensor.x
+            y = sensor.y
+            z = sensor.z
+        });
+        sensor.start();
+    } else {
+        $('#GyroscopeCheckbox').prop('disabled', true);
+        alert('Gyroscope not supported');
+    }
+
+}
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
@@ -62,6 +102,20 @@ function Model(name) {
 
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
     }
+
+    this.BufferDataSphere = function (vertices) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+        this.count = vertices.length / 3;
+    };
+
+    this.DrawSphere = function () {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
+    };
 }
 
 function ShaderProgram(name, program) {
@@ -82,32 +136,80 @@ function ShaderProgram(name, program) {
 }
 
 function draw() {
+    if (audioPanner) {
+        audioFilter.frequency.value = centerFrequencyInput;
+        audioFilter.Q.value = Q_value;
+        audio.volume = Volume;
+        audio.playbackRate = PlaybackRate;
+
+        if (GyroscopeRotate) {
+            audioPanner.setPosition(
+                (x * 1000).toFixed(2),
+                (y * 1000).toFixed(2),
+                (z * 1000).toFixed(2)
+            );
+        } else {
+            audioPanner.setPosition(
+                World_X * 1000,
+                World_Y * 1000,
+                World_Z * 1000
+            );
+        }
+    }
+
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    DrawWebCamVideo();
+    if (camera_ind) {
+        DrawWebCamVideo();
+    }
 
     gl.clear(gl.DEPTH_BUFFER_BIT);
     stereoCamera.ApplyLeftFrustum();
     gl.colorMask(true, false, false, false);
     DrawSurface();
+    DrawSphere();
 
     gl.clear(gl.DEPTH_BUFFER_BIT);
     stereoCamera.ApplyRightFrustum();
     gl.colorMask(false, true, true, false);
     DrawSurface();
+    DrawSphere();
     gl.colorMask(true, true, true, true);
 }
 
 function DrawSurface() {
-    let modelView = spaceball.getViewMatrix();
-    let translateToPointZero = m4.translation(World_X, World_Y, World_Z);
+    let modelView;
+    let translateToPointZero;
+    if (rotate_spaceball) {
+        modelView = spaceball.getViewMatrix();
+        translateToPointZero = m4.translation(0, 0, -20);
+    } else {
+        modelView = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+        translateToPointZero = m4.translation(-1, 0, -20);
+    }
 
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, m4.multiply(stereoCamera.mModelViewMatrix, m4.multiply(m4.multiply(stereoCamera.mProjectionMatrix, translateToPointZero), modelView)));
     gl.bindTexture(gl.TEXTURE_2D, SurfaceTexture);
     gl.uniform1i(shProgram.iTexture, 0);
 
     surface.Draw();
+}
+
+function DrawSphere() {
+    let modelView = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+    let translateToPointZero;
+    if (GyroscopeRotate) {
+        translateToPointZero = m4.translation(x.toFixed(2), y.toFixed(2), (z - 20).toFixed(2));
+    } else {
+        translateToPointZero = m4.translation(World_X, World_Y, World_Z - 20);
+    }
+
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, m4.multiply(stereoCamera.mModelViewMatrix, m4.multiply(m4.multiply(stereoCamera.mProjectionMatrix, translateToPointZero), modelView)));
+    gl.bindTexture(gl.TEXTURE_2D, SphereTexture);
+    gl.uniform1i(shProgram.iTexture, 0);
+
+    Sphere.Draw();
 }
 
 function DrawWebCamVideo() {
@@ -219,6 +321,9 @@ function initGL() {
     BackgroundVideoModel = new Model('Camera');
     let BackgroundData = CreateBackgroundData();
     BackgroundVideoModel.BufferData(BackgroundData[0], BackgroundData[1], BackgroundData[2]);
+
+    Sphere = new Model("Sphere");
+    Sphere.BufferDataSphere(CreateSphereSurface());
 }
 
 function CreateBackgroundData() {
@@ -232,6 +337,30 @@ function CreateBackgroundData() {
     let textCoords = [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1];
 
     return [vertexList, normalsList, textCoords];
+}
+
+function CreateSphereSurface(r = 0.15) {
+    let vertexList = [];
+    for (let lon = -Math.PI; lon < Math.PI; lon += 0.5) {
+        for (let lat = -Math.PI * 0.5; lat < Math.PI * 0.5; lat += 0.5) {
+            vertexList.push(
+                ...sphereSurfaceData(r, lon, lat),
+                ...sphereSurfaceData(r, lon + 0.5, lat),
+                ...sphereSurfaceData(r, lon, lat + 0.5),
+                ...sphereSurfaceData(r, lon + 0.5, lat + 0.5),
+                ...sphereSurfaceData(r, lon + 0.5, lat),
+                ...sphereSurfaceData(r, lon, lat + 0.5)
+            );
+        }
+    }
+    return vertexList;
+}
+
+function sphereSurfaceData(r, u, v) {
+    let x = r * Math.sin(u) * Math.cos(v);
+    let y = r * Math.sin(u) * Math.sin(v);
+    let z = r * Math.cos(u);
+    return [x, y, z];
 }
 
 function createProgram(gl, vShader, fShader) {
@@ -258,6 +387,7 @@ function createProgram(gl, vShader, fShader) {
 }
 
 function init() {
+    readGyroscope();
     try {
         canvas = document.getElementById("webglcanvas");
 
@@ -301,9 +431,9 @@ function init() {
     }, function (e) {
         console.error('Rejected!', e);
     });
-
     SetUpWebCamTexture();
     setTimeout(function () {
+        LoadSphereTexture();
         spaceball = new TrackballRotator(canvas, draw, 0);
 
         if (texture_type == "image") {
@@ -326,6 +456,81 @@ function init() {
             }
             requestAnimationFrame(render);
         }
+
+        audio = document.getElementById("audio");
+
+        audio.addEventListener("pause", () => {
+            audioContext.resume();
+        });
+
+        audio.addEventListener("play", () => {
+            if (!audioContext) {
+                audioContext = new(window.AudioContext || window.webkitAudioContext)();
+                audioSource = audioContext.createMediaElementSource(audio);
+
+                audioPanner = audioContext.createPanner();
+                audioFilter = audioContext.createBiquadFilter();
+                reverbNode = audioContext.createConvolver();
+
+                audioPanner.panningModel = "HRTF";
+                audioPanner.distanceModel = "linear";
+                audioFilter.type = "bandpass";
+                audioFilter.frequency.value = centerFrequencyInput;
+                audioFilter.Q.value = Q_value;
+
+                audio.volume = Volume;
+                audio.playbackRate = PlaybackRate;
+
+                const reverbUrl = "audio/reverb/BIG_HALL_E001_M2S.wav";
+                const reverbRequest = new XMLHttpRequest();
+                reverbRequest.open("GET", reverbUrl, true);
+                reverbRequest.responseType = "arraybuffer";
+
+                reverbRequest.onload = function () {
+                    const audioData = reverbRequest.response;
+                    audioContext.decodeAudioData(audioData, function (buffer) {
+                        reverbNode.buffer = buffer;
+                        audioSource.connect(audioPanner);
+                        audioPanner.connect(audioFilter);
+                        audioFilter.connect(reverbNode);
+                        reverbNode.connect(audioContext.destination);
+                    });
+                };
+
+                reverbRequest.send();
+
+                audioContext.resume();
+            }
+        });
+
+        const filter = document.getElementById("filter_check");
+
+        filter.addEventListener("change", function () {
+            if (filter.checked) {
+                audioPanner.disconnect();
+                audioPanner.connect(audioFilter);
+                audioFilter.connect(audioContext.destination);
+            } else {
+                audioPanner.disconnect();
+                audioPanner.connect(audioContext.destination);
+            }
+        });
+
+        const reverbCheckbox = document.getElementById('reverbCheckbox');
+
+        reverbCheckbox.addEventListener('change', () => {
+            if (reverbCheckbox.checked) {
+                audioFilter.disconnect();
+                audioFilter.connect(reverbNode);
+                reverbNode.connect(audioContext.destination);
+            } else {
+                reverbNode.disconnect();
+                audioFilter.disconnect();
+                audioFilter.connect(audioContext.destination);
+            }
+        });
+
+        audio.play();
 
         playVideo();
     }, 500);
@@ -433,6 +638,26 @@ function loadTexture(gl, url) {
     image.src = url;
 
     return SurfaceTexture;
+}
+
+function LoadSphereTexture() {
+    SphereTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, SphereTexture);
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+        new Uint8Array([0, 0, 255, 255]));
+
+    const SphereImage = new Image();
+    SphereImage.crossOrigin = "anonymus";
+    SphereImage.onload = () => {
+        gl.bindTexture(gl.TEXTURE_2D, SphereTexture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, SphereImage);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        draw();
+    };
+    SphereImage.src = "./texture/gold.avif";
 }
 
 function isPowerOf2(value) {
